90e207814985439a24a3df605e6412775d3856b1
Maximilian Welsch <maxiwelsch@aol.com>
2024-04-12 22:49:55 +0200

    simple webserver works

--- firmware_modem/src/main.cpp
+++ firmware_modem/src/main.cpp
@@ -1,23 +1,69 @@
+//
+// A simple server implementation showing how to:
+//  * serve static messages
+//  * read GET and POST parameters
+//  * handle missing pages / 404s
+//
+
 #include <Arduino.h>
-#include <HardwareSerial.h>
-
-#include <EEPROM.h>
-#include <LittleFS.h>
+#ifdef ESP32
+#include <WiFi.h>
 #include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
 #include <ESPAsyncWebServer.h>
-#include <NeoPixelBus.h>
 
+AsyncWebServer server(80);
 
-HardwareSerial SerialPort(2);
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
 
+const char* PARAM_MESSAGE = "message";
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
 
 void setup() {
-  Serial.begin(38400);
-  SerialPort.begin(38400, SERIAL_8N1, 18, 17);
+
+    Serial.begin(115200);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP("waberinator", NULL);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.softAPIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", "Hello, world");
+    });
+
+    // Send a GET request to <IP>/get?message=<message>
+    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+        String message;
+        if (request->hasParam(PARAM_MESSAGE)) {
+            message = request->getParam(PARAM_MESSAGE)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200, "text/plain", "Hello, GET: " + message);
+    });
+
+    // Send a POST request to <IP>/post with a form field message set to <message>
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+        String message;
+        if (request->hasParam(PARAM_MESSAGE, true)) {
+            message = request->getParam(PARAM_MESSAGE, true)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200, "text/plain", "Hello, POST: " + message);
+    });
+
+    server.onNotFound(notFound);
+
+    server.begin();
 }
 
 void loop() {
-  Serial.println("Bla0");
-  SerialPort.println("Bla1");
-  sleep(1);
 }
